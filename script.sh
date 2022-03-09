bdf60bb0044cbdf92a25114c096e7eb43e56dc9d
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2022-03-09 19:22:48 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo Upgrading System...
+sudo apt upadte && sudo pkcon -y update
+
+echo Installing: terminator, KeePassXC, htop, calc, gparted, neofetch, cmatrix, gimp, tty-clock, mpv, libreoffice, pv, torbrowser-launcher
+sudo apt install -y terminator keepass2 htop calc gparted neofetch cmatrix gimp tty-clock mpv libreoffice pv torbrowser-launcher
+
+echo Installing: zsh, zsh-syntax-highlighting, autojump, zsh-autosuggestions
+sudo apt install -y zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+
+echo Setting up ZSH
+touch "$HOME/.cache/zshhistory"
+cd $HOME/Downloads
+mkdir git
+cd git
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+sudo chsh --shell /bin/zsh $USER
+
+echo Starting Terminator
+echo You can close this window now...
+terminator &>/dev/null &
+disown %1
