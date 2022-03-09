472e0d8b64190372e4739eec25a19325e1cf6610
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2022-03-09 19:45:59 +0100

    Add Output when command finished

--- script.sh
+++ script.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 echo Upgrading System...
 sudo apt update &>/dev/null && sudo pkcon -y update &>/dev/null
+echo Finished!
+echo
 
 echo Installing: terminator, KeePassXC, htop, calc, gparted, neofetch, cmatrix, gimp, tty-clock, mpv, libreoffice, pv, torbrowser-launcher
 sudo apt install -y terminator keepass2 htop calc gparted neofetch cmatrix gimp tty-clock mpv libreoffice pv torbrowser-launcher &>/dev/null
+echo Finished!
+echo
 
 echo Installing: zsh, zsh-syntax-highlighting, autojump, zsh-autosuggestions
 sudo apt install -y zsh zsh-syntax-highlighting autojump zsh-autosuggestions &>/dev/null
+echo Finished!
+echo
 
 echo Setting up ZSH
 touch "$HOME/.cache/zshhistory"
@@ -16,6 +22,8 @@ cd git
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 sudo chsh --shell /bin/zsh $USER
+echo Finished!
+echo
 
 echo Starting Terminator
 echo You can close this window now...
